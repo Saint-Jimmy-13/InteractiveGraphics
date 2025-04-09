a220e39c9349db2ddf07062abdd1d94dbed43204
chris <pizzuti.1872078@studenti.uniroma1.it>
2025-04-10 01:05:04 +0200

    first attempt - not working

--- homework_1/project1_ComposingImages/project1.js
+++ homework_1/project1_ComposingImages/project1.js
@@ -3,5 +3,44 @@
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite(bgImg, fgImg, fgOpac, fgPos) {
-    
+    const bgData = bgImg.data;
+    const fgData = fgImg.data;
+
+    const bgWidth = bgImg.width;
+    const bgHeight = bgImg.height;
+    const fgWidth = fgImg.width;
+    const fgHeight = fgImg.height;
+
+    for (let y = 0; y < fgHeight; ++y) {
+        for (let x = 0; x < fgWidth; ++x) {
+            const bgX = fgPos.x + x;
+            const bgY = fgPos.y + y;
+
+            // Skip pixels outside the background image
+            if (bgX < 0 || bgX >= bgWidth || bgY < 0 || bgY >= bgHeight) {
+                continue;
+            }
+
+            const fgIdx = (y * fgWidth + x) * 4;
+            const bgIdx = (bgY * bgWidth + bgX) * 4;
+
+            const fgR = fgData[fgIdx + 0];
+            const fgG = fgData[fgIdx + 1];
+            const fgB = fgData[fgIdx + 2];
+            const fgA = fgData[fgIdx + 3];
+
+            const bgR = bgData[bgIdx];
+            const bgG = bgData[bgIdx + 1];
+            const bgB = bgData[bgIdx + 2];
+            const bgA = bgData[bgIdx + 3];
+
+            const totA = fgA + (1 - fgA) * bgA;
+
+            // Alpha Blending
+            bgData[bgIdx + 0] = (fgA * fgR + (1 - fgA) * bgA * bgR) / totA; // Red
+            bgData[bgIdx + 1] = (fgA * fgG + (1 - fgA) * bgA * bgG) / totA; // Green
+            bgData[bgIdx + 2] = (fgA * fgB + (1 - fgA) * bgA * bgB) / totA; // Blue
+            bgData[bgIdx + 3] = totA;
+        }
+    }
 }
