ef8bb6d0fc53af843efda511ed6ad3214ca9a27a
chris <pizzuti.1872078@studenti.uniroma1.it>
2025-04-10 13:48:29 +0200

    debugging

--- homework_1/project1_ComposingImages/project1.js
+++ homework_1/project1_ComposingImages/project1.js
@@ -6,6 +6,9 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
     const bgData = bgImg.data;
     const fgData = fgImg.data;
 
+    console.log(bgData);
+    console.log(fgData);
+
     const bgWidth = bgImg.width;
     const bgHeight = bgImg.height;
     const fgWidth = fgImg.width;
@@ -27,20 +30,28 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
             const fgR = fgData[fgIdx + 0];
             const fgG = fgData[fgIdx + 1];
             const fgB = fgData[fgIdx + 2];
-            const fgA = fgData[fgIdx + 3];
+            const fgA = (fgData[fgIdx + 3] / 255 ) * fgOpac;
 
             const bgR = bgData[bgIdx];
             const bgG = bgData[bgIdx + 1];
             const bgB = bgData[bgIdx + 2];
-            const bgA = bgData[bgIdx + 3];
+            const bgA = bgData[bgIdx + 3] / 255;
 
+            // alpha = alpha_f + (1 - alpha_f) * alpha_b
             const totA = fgA + (1 - fgA) * bgA;
 
-            // Alpha Blending
-            bgData[bgIdx + 0] = (fgA * fgR + (1 - fgA) * bgA * bgR) / totA; // Red
-            bgData[bgIdx + 1] = (fgA * fgG + (1 - fgA) * bgA * bgG) / totA; // Green
-            bgData[bgIdx + 2] = (fgA * fgB + (1 - fgA) * bgA * bgB) / totA; // Blue
-            bgData[bgIdx + 3] = totA;
+            // Avoid division by zero
+            if (totA === 0) {
+                continue;
+            }
+
+            // Alpha Blending: (alpha_f * c_f + (1 - alpha_f) * alpha_b * c_b) /
+            bgData[bgIdx + 0] = Math.round((fgA * fgR + (1 - fgA) * bgA * bgR) / totA); // Red
+            bgData[bgIdx + 1] = Math.round((fgA * fgG + (1 - fgA) * bgA * bgG) / totA); // Green
+            bgData[bgIdx + 2] = Math.round((fgA * fgB + (1 - fgA) * bgA * bgB) / totA); // Blue
+            bgData[bgIdx + 3] = Math.round(totA);   // Alpha
         }
     }
+    console.log(bgData);
+    console.log(fgData);
 }
