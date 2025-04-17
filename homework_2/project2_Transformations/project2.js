dbb75656f2a57ecb15e9eab9731fb48cd442857a
chris <pizzuti.1872078@studenti.uniroma1.it>
2025-04-17 23:35:27 +0200

    hw2 done

--- homework_2/project2_Transformations/project2.js
+++ homework_2/project2_Transformations/project2.js
@@ -32,7 +32,17 @@ function GetTransform(positionX, positionY, rotation, scale) {
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
 // The arguments are transformation matrices in the same format.
 // The returned transformation first applies trans1 and then trans2.
-function ApplyTransform( trans1, trans2 )
-{
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+function ApplyTransform(trans1, trans2) {
+	var ret = new Array(9);
+	
+	for (let r = 0; r < 3; ++r) {
+		for (let c = 0; c < 3; ++c) {
+			let sum = 0;
+			for (let k = 0; k < 3; ++k) {
+				sum += trans2[r + 3 * k] * trans1[k + 3 * c];
+			}
+			ret[r + 3 * c] = sum;
+		}
+	}
+	return ret;
 }
