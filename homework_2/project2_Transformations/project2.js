da697cee1d35e451ce942a2b90a02e43e6d7dfe9
chris <pizzuti.1872078@studenti.uniroma1.it>
2025-04-17 23:12:54 +0200

    half done

--- homework_2/project2_Transformations/project2.js
+++ homework_2/project2_Transformations/project2.js
@@ -1,9 +1,32 @@
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
 // The transformation first applies scale, then rotation, and finally translation.
 // The given rotation value is in degrees.
-function GetTransform( positionX, positionY, rotation, scale )
-{
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+function GetTransform(positionX, positionY, rotation, scale) {
+	// Convert rotation to radians
+	var rad = rotation * Math.PI / 180.0;
+	var cos = Math.cos(rad);
+	var sin = Math.sin(rad);
+
+	// The matrix we want is:
+	//
+	// [cos*scale	-sin*scale	positionX]
+	// [sin*scale	 cos*scale	positionY]
+	// [	0			0			1	 ]
+	//
+	// Where the first and second columns contain rotation and scale part,
+	// and third column contains translation part.
+
+	return [
+		cos * scale,	// array[0]
+		sin * scale,	// array[1]
+		0,				// array[2]
+		-sin * scale,	// array[3]
+		cos * scale,	// array[4]
+		0,				// array[5]
+		positionX,		// array[6]
+		positionY,		// array[7]
+		1,				// array[8]
+	];
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
